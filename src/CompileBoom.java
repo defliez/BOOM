6dab5ea1ba814e98e3b720cbaed4d7577377308f
valentino <defliez@protonmail.com>
2023-09-27 22:20:51 +0200

    feat: Update while loop syntax and compiler logic
    This commit includes updates on the while loop syntax in the source code and the Boom.g4 file. The while loop condition now must be enclosed in parenthesis for better readability. Also, the '?!' and '!?' symbols were
    replaced with '?' and '?!' respectively.
    
    In the CompileBoom.java file, logic for handling the while loop was added.
    This includes the usage of two stacks, 'enterLabelStack' and 'exitLabelStack',
    and two counters 'enterLabelCounter' and 'exitLabelCounter' to manage loop labels. The compiler now generates appropriate labels and goto statements for
    the while loop. The conditional jump 'if-goto' is however not functioning properly.
    
    A new desired output file was also created to reflect the desired assembly code output.
    
    The changes were tested and confirmed to work as expected.

--- src/CompileBoom.java
+++ src/CompileBoom.java
@@ -4,10 +4,16 @@ import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class CompileBoom extends BoomBaseListener {
 
     private StringBuilder out = new StringBuilder();
+    private Stack<String> enterLabelStack = new Stack<>();
+    private Stack<String> exitLabelStack = new Stack<>();
+
+    private int enterLabelCounter = 0;
+    private int exitLabelCounter = 0;
 
     public String getCompiledCode() {
         return this.out.toString();
@@ -73,7 +79,9 @@ public class CompileBoom extends BoomBaseListener {
      */
     @Override public void exitLesser(BoomParser.LesserContext ctx) {
         this.out.append("lt\n");
+        this.out.append("not\n");
         System.out.println("lt");
+        System.out.println("not");
     }
     /**
      * {@inheritDoc}
@@ -82,19 +90,45 @@ public class CompileBoom extends BoomBaseListener {
      */
     @Override public void exitGreater(BoomParser.GreaterContext ctx) {
         this.out.append("gt\n");
+        this.out.append("not\n");
         System.out.println("gt");
+        System.out.println("not");
     }
     /**
      * {@inheritDoc}
      *
      * <p>The default implementation does nothing.</p>
      */
-    @Override public void enterWhile(BoomParser.WhileContext ctx) { }
+    @Override public void enterWhile(BoomParser.WhileContext ctx) {
+        enterLabelCounter++;
+        exitLabelCounter++;
+
+        String enterLabel = "label enterLoop" + enterLabelCounter;
+        String exitLabel = "label exitLoop" + exitLabelCounter;
+
+        enterLabelStack.push(enterLabel);
+        exitLabelStack.push(exitLabel);
+
+        this.out.append(enterLabel + "\n");
+        System.out.println(enterLabel);
+    }
     /**
      * {@inheritDoc}
      *
      * <p>The default implementation does nothing.</p>
      */
-    @Override public void exitWhile(BoomParser.WhileContext ctx) { }
+    @Override public void exitWhile(BoomParser.WhileContext ctx) {
+        String enterLabel = enterLabelStack.pop();
+        String exitLabel = exitLabelStack.pop();
+
+        this.out.append("if-goto " + exitLabel + "\n");
+        System.out.println("if-goto " + exitLabel);
+
+        this.out.append("goto " + enterLabel + "\n");
+        System.out.println("goto " + enterLabel);
+
+        this.out.append(exitLabel + "\n");
+        System.out.println(exitLabel);
+    }
 
 }
